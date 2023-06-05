f4b0540fcc9ca7d231cfeb6095a1fe3fc3024de1
dfernandonoguera <dnogueragoyes@itba.edu.ar>
2023-06-05 14:28:26 -0300

    05-06-2023-exp4v2

--- src/workflow-semillerio/751_HT_lightgbm-04.r
+++ src/workflow-semillerio/751_HT_lightgbm-04.r
@@ -31,9 +31,9 @@ options(error = function() {
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento  <- "HT7510-03"
+PARAM$experimento  <- "HT7510-04"
 
-PARAM$exp_input  <- "TS7410-03"
+PARAM$exp_input  <- "TS7410-04"
 
 PARAM$lgb_crossvalidation_folds  <- 5  #En caso que se haga cross validation, se usa esta cantidad de folds
 
